efdef50bebebc04a99803327f2faac3945728304
The Exorcist <whitebeard086@gmail.com>
2022-08-12 21:15:07 +0100

    initialize an express server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log('Hello Api!');
+/* eslint-disable linebreak-style */
+import express from 'express';
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`);
+});
