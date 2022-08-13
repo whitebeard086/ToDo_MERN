306bdd07e740646cab9e4d53c48cc2e002cc51a9
The Exorcist <whitebeard086@gmail.com>
2022-08-13 20:13:14 +0100

    Express.js middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 8000;
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser);
 
 const connectDB = async () => {
   try {
