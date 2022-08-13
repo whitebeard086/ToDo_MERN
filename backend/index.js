999947de04069e626442c1d11a545e04197679a1
The Exorcist <whitebeard086@gmail.com>
2022-08-13 09:18:34 +0100

    connect mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server is running on port: ${PORT}`);
 });
