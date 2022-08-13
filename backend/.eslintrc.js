e64447f972db2da0e93b125eed3932c637f2627e
The Exorcist <whitebeard086@gmail.com>
2022-08-13 20:00:26 +0100

    task and user models

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   },
   rules: {
     'no-console': 0,
+    'linebreak-style': 0,
   },
 };
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
